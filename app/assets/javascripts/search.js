01d1435c69dd931386654599692eabfb0dc6c837
yufauefa <yuto.program@gmail.com>
2019-08-05 16:09:58 +0900

    とりあえず

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -8,10 +8,16 @@ $(function() {
     return html;
   }
 
-
+  function appendErrMsgToHTML(msg){
+    var html =`<div class="chat-group-user clearfix">
+                <p> ${msg} </p>
+              </div>`
+    $('#user-search-result').append(html);
+  }
 
 
   $('#user-search-field').on('keyup', function(){
+    $('#user-search-result').empty();
     var input = $('#user-search-field').val();
     $.ajax({
       type: 'GET',
@@ -20,17 +26,30 @@ $(function() {
       dataType: 'json'
     })
     .done(function(users){
+      if (users.length !== 0){
       users.forEach(function(user){
         var html = appendUser(user);
         $('#user-search-result').append(html);
       });
+     }
+     else {
+       appendErrMsgToHTML("一致する情報はありません");
+     }
     })
     .fail(function(){
       alert('error');
     })
   });
 
-  
+  // $('#user-search-field').on('keyup', function(){
+  //   if (users !== 0 ){
+  //     search(users);
+  //   }
+  //   else{
+  //   function(){
+  //     alert('error');
+  //   }
+  // });
 
 
 
