e9c00cc9fc1ad0bbe5c9f039187aaf7abc6b3576
yufauefa <yuto.program@gmail.com>
2019-08-05 11:04:06 +0900

    0805

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -3,9 +3,9 @@ $(function() {
   function appendUser(user){
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
-                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
                 </div>`
-    $('.user-search-result').append(html);
+    $('.edit_group').append(html);
   }
 
 
@@ -13,7 +13,7 @@ $(function() {
 
   $('#user-search-field').on('keydown', function(){
     var input = $('#user-search-field').val();
-
+    var reg = new RegExp("^" + input );
     $.ajax({
       type: 'GET',
       url: '/users',
@@ -21,8 +21,9 @@ $(function() {
       dataType: 'json'
     })
     .done(function(users){
-      console.log(users);
-      })
+      users.forEach(function(user){
+        appendUser(user);
+      });
     })
     .fail(function(){
       alert('error');
