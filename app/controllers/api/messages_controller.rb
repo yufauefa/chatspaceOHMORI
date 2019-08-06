92a570e3b537b792b2fe685d5371494a40cc610b
yufauefa <yuto.program@gmail.com>
2019-08-06 12:59:03 +0900

    コントローラー

---
+++ app/controllers/api/messages_controller.rb
@@ -0,0 +1,9 @@
+class Api::MessagesController < ApplicationController
+  def index
+    @message= Message.all
+    respond_to do | format |
+      format.html
+      format.json { @new_message = Message.where('id > ?', params[:message][:id] )}
+    end
+  end
+end
